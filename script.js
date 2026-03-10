1c18ede6fe8193d156b45390d724106fdf8e111d
Vass Dávid Róbert <125920552+thefreezecroc@users.noreply.github.com>
2026-03-10 13:15:24 +0100

    Kesz

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const url = "https://hexarate.paikama.co/api/rates/USD/GBP/latest";
-
-fetch(url)
-.then()
+function atvaltas() {
+    const valuta = document.getElementById("lista").value;
+    const osszeg = document.getElementById("osszeg").value;
+    const url = `https://hexarate.paikama.co/api/rates/HUF/${valuta}/latest`;
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("hiba a kérésben");
+            }
+            return response.json();
+        })
+        .then(json => {
+          let result = osszeg * json.data.mid;
+           const p = document.getElementById("vegOsszeg");
+           p.innerHTML = `${osszeg} HUF = ${result.toFixed(2)} ${valuta}`;
+        })
+}
